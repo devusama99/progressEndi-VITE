1dff5aa7f0084e11cd2f82c1096009f55a52ec5b
Muhammad Usama <dev.usama99@gmail.com>
2023-04-12 20:49:51 +0500

    Scrollbars issue resolved and also video tutorials

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/router";
 import { theme } from "./styles/theme";
-import "mac-scrollbar/dist/mac-scrollbar.css";
 
 function App() {
   return (
